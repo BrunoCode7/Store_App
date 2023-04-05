67e4b02c7717bf5d3cf857be09a317f41314d90e
Baraa BELAL <baraa.belal@flexxpay.com>
2023-04-05 23:54:12 +0200

    Added mockito library for unit tests and also added 2 unit tests for domain layer use cases classes

---
+++ product-domain/src/test/java/com/example/products_domain/GetProductsListUseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.example.products_domain
+
+import com.example.products_domain.entity.product.Product
+import com.example.products_domain.entity.product.Rating
+import com.example.products_domain.repository.ProductRepository
+import com.example.products_domain.usecase.GetProductsListUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.*
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+
+/**
+ * test case for GetProductsListUseCase class
+ */
+
+class GetProductsListUseCaseTest {
+    private val productRepository = mock<ProductRepository>()
+    private val useCase = GetProductsListUseCase(
+        configuration = mock(),
+        productRepository = productRepository
+    )
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testProcess()  = runBlockingTest {
+        val product1 = Product(
+            "test1","test1",1,"test1",1.0, Rating(1,1.0),"title1"
+        )
+        val product2 = Product(
+            "test2","test2",2,"test2",2.0, Rating(2,2.0),"title2"
+        )
+        val product3 = Product(
+            "test3","test3",3,"test3",3.0, Rating(3,3.0),"title3"
+        )
+
+        whenever(productRepository.getProducts()).thenReturn(flowOf(listOf(product1,product2,product3)))
+
+        val response = useCase.process(GetProductsListUseCase.Request).first()
+
+        assertEquals(GetProductsListUseCase.Response(listOf(product1,product2,product3)), response)
+
+    }
+
+
+}
---
+++ product-domain/src/test/java/com/example/products_domain/UseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.example.products_domain
+
+import com.example.products_domain.entity.Result.*
+import com.example.products_domain.usecase.UseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.Before
+import org.mockito.kotlin.mock
+
+/**
+ * test case for base use case class
+ */
+class UseCaseTest {
+
+    @ExperimentalCoroutinesApi
+    private val configuration = UseCase.Configuration(TestCoroutineDispatcher())
+    private val request = mock<UseCase.Request>()
+    private val response = mock<UseCase.Response>()
+
+    @ExperimentalCoroutinesApi
+    private lateinit var useCase:
+            UseCase<UseCase.Request, UseCase.Response>
+
+    @ExperimentalCoroutinesApi
+    @Before
+    fun setUp() {
+        useCase = object : UseCase<UseCase.Request,
+                UseCase.Response>(configuration) {
+            override fun process(request: Request):
+                    Flow<Response> {
+                assertEquals(this@UseCaseTest.request,
+                    request)
+                return flowOf(response)
+            }
+        }
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testExecuteSuccess() = runBlockingTest {
+        val result = useCase.execute(request).first()
+        assertEquals(Success(response), result)
+    }
+}
